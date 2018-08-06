89bd538f2ec3192580c0a96162e206e770f812da
Hao Tran <git@haothitran.com>
2018-08-06 13:35:52 -0400

    Fix if statement, do not announce others interrupts

--- Modules/Interrupt.lua
+++ Modules/Interrupt.lua
@@ -9,9 +9,9 @@ if C.Interrupt.Self ~= true then return end
 
 local f = CreateFrame("Frame")
 f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-f:SetScript("OnEvent", function(self, _, ...)
+f:SetScript("OnEvent", function(self)
   local _, event, _, sourceGUID, _, _, _, _, destName, _, _, _, _, _, spellID = CombatLogGetCurrentEventInfo()
-  if not event == "SPELL_INTERRUPT" and not sourceGUID == UnitGUID("player") then return end
+  if not (event == "SPELL_INTERRUPT" and sourceGUID == UnitGUID("player")) then return end
 
   if C.Interrupt.Say == true then
     local _, instanceType = IsInInstance()
