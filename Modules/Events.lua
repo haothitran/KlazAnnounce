c87d28ed6b6e8c653c0d33b3c777c1b0d2ea9860
Hao Tran <git@haothitran.com>
2018-08-07 13:14:39 -0400

    Add player condition to event announcements

--- Modules/Events.lua
+++ Modules/Events.lua
@@ -11,31 +11,31 @@ if C.Events ~= true then return end
 local f = CreateFrame("Frame")
 f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 f:SetScript("OnEvent", function(self)
-	local _, event, _, sourceGUID, srcName, _, _, _, destName, _, _, spellID = CombatLogGetCurrentEventInfo()
-	if InCombatLockdown() or not IsInGroup() or not subEvent or not spellID or not srcName then return end
-	if not UnitInRaid(srcName) and not UnitInParty(srcName) then return end
+	local _, event, _, _, sourceName, _, _, _, _, _, _, spellID = CombatLogGetCurrentEventInfo()
+	if InCombatLockdown() or not IsInGroup() or not event or not spellID or not sourceName then return end
+	if not UnitInRaid(sourceName) and not UnitInParty(sourceName) then return end
 
-	local srcName = srcName:gsub("%-[^|]+", "")
+	local sourceName = sourceName:gsub("%-[^|]+", "")
 	if event == "SPELL_CAST_SUCCESS" then
 		-- Ritual of Summoning
-    if spellID == 698 and sourceGUID == UnitGUID("player") then
-      SendChatMessage(srcName.." "..L.EVENTS_CAST.." "..GetSpellLink(spellID)..". "..L.EVENTS_CLICK.."!", T.ChatChannel)
+    if spellID == 698 and sourceName == UnitName("player") then
+      SendChatMessage(sourceName.." "..L.EVENTS_CAST.." "..GetSpellLink(spellID)..". "..L.EVENTS_CLICK.."!", T.ChatChannel)
     -- Spirit Cauldron
     elseif spellID == 188036 then
-			SendChatMessage(srcName.." "..L.EVENTS_PREPARE.." "..GetSpellLink(spellID)..".", T.ChatChannel)
+			SendChatMessage(sourceName.." "..L.EVENTS_PREPARE.." "..GetSpellLink(spellID)..".", T.ChatChannel)
     end
 	elseif event == "SPELL_SUMMON" then
 		-- bots (e.g. Jeeves, Blingtron)
 		if T.FilterEvents[spellID] then
-      SendChatMessage(srcName.." "..L.EVENTS_PLACE.." "..GetSpellLink(spellID)..".", T.ChatChannel)
+      SendChatMessage(sourceName.." "..L.EVENTS_PLACE.." "..GetSpellLink(spellID)..".", T.ChatChannel)
 		end
 	elseif event == "SPELL_CREATE" then
-		-- Ritual of Souls
-		if spellID == 29893 and sourceGUID == UnitGUID("player") then
-      SendChatMessage(srcName.." "..L.EVENTS.PLACE.." "..GetSpellLink(spellID)..".", T.ChatChannel)
+		-- Create Soulwell
+		if spellID == 29893 and sourceName == UnitName("player") then
+      SendChatMessage(sourceName.." "..L.EVENTS_PLACE.." "..GetSpellLink(spellID)..".", T.ChatChannel)
 		-- MOLLE-E
     elseif spellID == 54710 then
-      SendChatMessage(srcName.." "..L.EVENTS.PLACE.." "..GetSpellLink(spellID)..".", T.ChatChannel)
+      SendChatMessage(sourceName.." "..L.EVENTS_PLACE.." "..GetSpellLink(spellID)..".", T.ChatChannel)
     end
 	end
 end)
