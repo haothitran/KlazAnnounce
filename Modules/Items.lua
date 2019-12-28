8e7f077f6c027c981a939020d835b5d1e102296f
Hao Tran <account@haothitran.com>
2019-12-28 01:56:38 -0500

    Rename variable from event to subEvent

--- Modules/Items.lua
+++ Modules/Items.lua
@@ -10,13 +10,13 @@ if C.Items ~= true then return end
 
 local f = CreateFrame('Frame')
 f:RegisterEvent('COMBAT_LOG_EVENT_UNFILTERED')
-f:SetScript('OnEvent', function(self)
-  local _, event, _, _, sourceName, _, _, _, _, _, _, spellID = CombatLogGetCurrentEventInfo()
-  if InCombatLockdown() or not IsInGroup() or not event or not spellID or not sourceName then return end
+f:SetScript('OnEvent', function()
+  local _, subEvent, _, _, sourceName, _, _, _, _, _, _, spellID = CombatLogGetCurrentEventInfo()
+  if InCombatLockdown() or not IsInGroup() or not subEvent or not spellID or not sourceName then return end
   if not UnitInRaid(sourceName) and not UnitInParty(sourceName) then return end
 
   local sourceName = sourceName:gsub('%-[^|]+', '')
-  if event == 'SPELL_CAST_SUCCESS' then
+  if subEvent == 'SPELL_CAST_SUCCESS' then
     -- Ritual of Summoning
     if spellID == 698 and sourceName == UnitName('player') then
       SendChatMessage(sourceName..' '..L.ITEMS_CAST..' '..GetSpellLink(spellID)..'. '..L.ITEMS_CLICK..'!', T.ChatChannel(true))
@@ -24,12 +24,12 @@ f:SetScript('OnEvent', function(self)
   elseif T.FilterItemsCast[spellID] then
       SendChatMessage(sourceName..' '..L.ITEMS_PREPARE..' '..GetSpellLink(spellID)..'.', T.ChatChannel(true))
     end
-  elseif event == 'SPELL_SUMMON' then
+  elseif subEvent == 'SPELL_SUMMON' then
     -- bots (e.g. Jeeves, Blingtron)
     if T.FilterItemsSummon[spellID] then
       SendChatMessage(sourceName..' '..L.ITEMS_PLACE..' '..GetSpellLink(spellID)..'.', T.ChatChannel(true))
     end
-  elseif event == 'SPELL_CREATE' then
+  elseif subEvent == 'SPELL_CREATE' then
     -- Create Soulwell
     if spellID == 29893 and sourceName == UnitName('player') then
       SendChatMessage(sourceName..' '..L.ITEMS_PLACE..' '..GetSpellLink(spellID)..'.', T.ChatChannel(true))
